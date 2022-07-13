5fb1fb3efd399f9844dff3d2c9be13bc9d2dd657
Alejo <alejoivanrojas@gmail.com>
2022-07-13 10:43:40 -0300

    grid done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,25 @@
+import { Box, Grid } from '@mui/material'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import BasicCard from '../features/Card'
-import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className={styles.container}>
-        <BasicCard materia='Infraestructura IV'/>
-    </div>
+      <Grid container spacing={4} p="2rem" justifyContent="center">
+        <Grid item md={5} >
+          <BasicCard materia='Infraestructura IV'/>
+        </Grid>
+        <Grid item md={5} >
+          <BasicCard materia='Especializcion Frontend VI'/>
+        </Grid>
+        <Grid item  md={5}>
+          <BasicCard materia='#WELCOMEABOARD!'/>
+        </Grid>
+        <Grid item  md={5}>
+          <BasicCard materia='Testing I'/>
+        </Grid>
+      </Grid>
+
   )
 }
 
